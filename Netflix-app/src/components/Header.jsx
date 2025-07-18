f2ee65ffcfb1d28acba73c0d0ef8f4c649781452
muhammed rila <muhammedrilak1332@gamil.com>
2025-07-18 15:43:44 +0530

    feat: Initialize Netflix app with React and Vite setup
    - Added package.json with dependencies and scripts for development.
    - Included Vite logo SVG in the public directory.
    - Created App component with basic structure and state management.
    - Added React logo SVG in assets.
    - Implemented Footer component with contact information and links.
    - Developed Header component with logo and sign-in button.
    - Styled Footer and Header components with respective CSS files.
    - Set up main CSS for global styles.
    - Configured Vite for React application.

---
+++ Netflix-app/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+        <div className='header'>
+            <div className="logo">
+                <img src="./images/logo.png" alt="logo" />
+            </div>
+
+            <div className="content">
+                <select name="" id="" className='dropdown'>
+                  <option value=""><a href="">English</a></option>
+                  <option value=""><a href="">हिन्दी</a></option>
+                </select>
+                <button className='btn'><a href="">Sign In</a></button>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
