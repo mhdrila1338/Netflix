1ccb606db927546a03637b87d2e47e4d29d0be4f
muhammed rila <muhammedrilak1332@gamil.com>
2025-07-19 13:33:03 +0530

    updated

--- Netflix-app/src/main.jsx
+++ Netflix-app/src/main.jsx
@@ -5,10 +5,12 @@ import App from './App.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/footer.jsx'
 import './main.css'
+import Main_center from './components/main_center.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <Main_center />
     <App />
     <Footer />
   </StrictMode>,
